603bd71b0ae09da7c39425a0f2c4faa2388ea6d0
TXJTbWl0aA== <32136907+TheLastMonkey@users.noreply.github.com>
2018-08-11 19:08:16 -0500

    v2
    Make it more better-er!

---
+++ PASTE_O_MATICv2.py
@@ -0,0 +1,283 @@
+import os
+import pyautogui
+import clipboard
+import time
+from ast import literal_eval
+
+dict_of_info = {}
+text_repeated = ''
+
+
+def loopy():
+    global dict_of_info, text_repeated
+    text_repeated = dict_of_info['text']
+    temp = dict_of_info['text']
+    num = dict_of_info['times_to_repeat'] - 1  # -1 because of original
+
+    for the_loop in range(num):
+
+        if dict_of_info['same_or_next'] in 's':
+            text_repeated += temp
+            #  print(dict_of_info['times_to_repeat'])
+
+        elif dict_of_info['same_or_next'] in 'n':
+            text_repeated += "\n"
+            text_repeated += temp   # see if this can be cleaned up
+
+
+######################################
+def do_next():
+    do_this_next = input('[M]enu or [Q]uit : ')
+    if do_this_next in ('m', ''):
+        main()
+    elif do_this_next in 'q':
+        quit()
+    else:
+        quit()
+
+
+######################################
+def clipboard_mode():
+    os.system("clear")
+    print('Clipboard Mode'.center(50, '_'), '\n')
+    global dict_of_info, text_repeated
+    loopy()
+    clipboard.copy(text_repeated)
+    print('Copied')
+    do_next()
+
+
+def file_mode():
+    global dict_of_info, text_repeated
+    os.system("clear")
+    print('File Mode'.center(50, '_'), '\n')
+    loopy()
+    file_name = input('Input a File Name with the file ext ie [file.txt] : ')
+    try:
+        open_file = open(file_name, "w+")
+        open_file.write(text_repeated)
+        open_file.close()
+        print('Saved to ', file_name)
+        do_next()
+    except FileNotFoundError as e:
+        print(e)
+        print('You need to type a file name there...')
+        time.sleep(2)
+        what_do_want_to_do()
+
+
+def save_mode():
+    global dict_of_info, text_repeated
+    os.system("clear")
+    print('Saving'.center(50, '_'), '\n')
+    open_config_file = open("config.txt", "a")
+    open_config_file.write(str(dict_of_info) + "\n")
+    open_config_file.close()
+    print('Saved!')
+    do_next()
+
+
+def spam_mode():
+    os.system("clear")
+    global dict_of_info, text_repeated
+    print('SPAM MODE! Menu'.center(50, '_'), '\n')
+    print('First let me get some info')
+    my_interval = float(input("\nInput the interval at which to type each character [try 0.02] # : "))
+    delay = float(input("\nInput a delay between pastes [0 for no delay] : "))
+    time_till_spam = float(input("\nNumber of sec till spamming starts? : "))
+    print('Key'.center(50, '_'), '\n')
+    print(f"[R]epeated = [Your text] Repeated {dict_of_info['times_to_repeat']} times\n")
+    print(f"[S]pam = Repeated * # you will input\n")
+    print('End of Key'.center(50, '_'), '\n')
+    spam_or_repeat = input('Would you like to use [R]epeated or [S]pam \n')
+
+    if spam_or_repeat in 'r':
+        os.system("clear")
+        print('SPAM MODE! Menu'.center(50, '_'), '\n')
+        print(f"[Your text] will be repeated {dict_of_info['times_to_repeat']} times\n")
+        print("A Return Character will be played a the End of [Your Text Repeated] as a 'Send' \n")
+        loopy()
+
+        def repeat_loop():
+            go_on = input("press [Enter] to Start : ")  # dumpy input var to just wait for input find better way
+            print("\n[TIMER STARTED!]")
+            time.sleep(time_till_spam)
+
+            if dict_of_info['same_or_next'] in 's':
+                pyautogui.typewrite(text_repeated, interval=my_interval)
+                time.sleep(delay)
+                pyautogui.press('enter')
+
+            elif dict_of_info['same_or_next'] in 'n':
+                for loop in range(dict_of_info['times_to_repeat']):
+                    pyautogui.typewrite(dict_of_info['text'], interval=my_interval)
+                    pyautogui.press('enter')
+                    time.sleep(delay)
+            run_again = input('Run it Again?    [Enter]     [Q]uit      [M]enu : ')
+            if run_again in ("", 'y'):
+                repeat_loop()
+            elif run_again in "q":
+                print('Bye!')
+                quit()
+            elif run_again in 'm':
+                what_do_want_to_do()
+
+        repeat_loop()
+
+    elif spam_or_repeat in 's':
+        os.system("clear")
+        print('SPAM MODE! Menu'.center(50, '_'), '\n')
+        print(f"OK! [Your Text Repeated]{dict_of_info['times_to_repeat']} times should be spammed how many time?")
+        loopy()
+        print("A Return Character will be played a the END of [Your Text Repeated] as a 'Send' \n")
+        times_to_spam = int(input("Times to be Spammed : "))
+
+        def spam_loop():
+            go_on = input("press [Enter] to Start : ")
+            print("\n[TIMER STARTED!]")
+            time.sleep(time_till_spam)
+            for loop in range(times_to_spam):
+                pyautogui.typewrite(text_repeated, interval=my_interval)
+                pyautogui.press('enter')
+                time.sleep(delay)
+            run_again = input('Run it Again?    [Enter]     [Q]uit      [M]enu : ')
+            if run_again in ("", 'y'):
+                spam_loop()
+            elif run_again in "q":
+                print('Bye!')
+                quit()
+            elif run_again in 'm':
+                what_do_want_to_do()
+
+        spam_loop()
+
+
+def print_here():
+    global dict_of_info, text_repeated
+    os.system("clear")
+    print('Print Here'.center(50, '_'), '\n')
+    loopy()
+    print(text_repeated)
+    do_next()
+
+
+######################################
+def what_do_want_to_do():
+    global dict_of_info
+    os.system("clear")
+    print('What Do You Want to Do'.center(50, '_'), '\n')
+    print(f"OK I'll Repeat that {dict_of_info['times_to_repeat']} Times \n")
+    print('How to Output it')
+    print('1. [C]lipboard')
+    print('2. [F]ile.txt')
+    print('3. [S]ave For Later')
+    print('4. [J]ust SPAM IT!')
+    print('5. [P]rint Here...')
+    print('6. [M]ain Menu ')
+    output_choice = input('Your Output Choice : ')
+
+    if output_choice in ('1', 'c'):
+        clipboard_mode()
+    elif output_choice in ('2', 'f'):
+        file_mode()
+    elif output_choice in ('3', 's'):
+        save_mode()
+    elif output_choice in ('4', 'j'):
+        spam_mode()
+    elif output_choice in ('5', 'p'):
+        print_here()
+    elif output_choice in ('6', 'm'):
+        main()
+
+
+def add_text():
+    global dict_of_info
+    os.system("clear")
+    print('Input Some Text to Repeat'.center(50, '_'), '\n')
+    text = input('Input some text : ')
+    times_to_repeat = int(input('How Many Times Do You Want That Repeated : '))
+    same_or_next = input('Repeat on [S]ame or [N]ext Line : ')
+    dict_of_info['text'] = text
+    dict_of_info['times_to_repeat'] = times_to_repeat
+    dict_of_info['same_or_next'] = same_or_next
+    print(dict_of_info)
+    what_do_want_to_do()
+
+
+def view_saved():
+    global dict_of_info
+    os.system("clear")
+    print('View Saved'.center(50, '_'), '\n')
+    try:
+        open_read_config_file = open("config.txt", "r")
+        for index, line in enumerate(open_read_config_file, start=1):
+            print(index, line)
+        print()
+        my_saved_config_choice = input("Press [Enter] to return to main menu or pick a [#] to use : ")
+        # print(type(my_saved_config_choice))
+        try:
+            if my_saved_config_choice in '':
+                main()
+            else:
+                try:
+                    my_saved_config_choice = int(my_saved_config_choice)
+                    # print(type(my_saved_config_choice))
+                except ValueError as e:
+                    print('!!!ERROR!!!', e)
+                    print("So That's NOT a Number")
+                    time.sleep(2)
+                    view_saved()
+            open_read_config_file = open("config.txt", "r")
+            print('You want #', my_saved_config_choice)
+            my_saved_config_choice -= 1
+            lines = open_read_config_file.readlines()
+            # print(lines)
+            open_read_config_file.close()
+            print(lines[my_saved_config_choice])
+            use_me = lines[my_saved_config_choice]
+
+            dict_of_info = literal_eval(use_me)
+            what_do_want_to_do()
+        except IndexError as e:
+            print('!!!ERROR!!!', e)
+            print('That Number Dose NOT Exist')
+            do_next()
+
+    except FileNotFoundError as e:
+        print(e, '\nYou do have a config.txt yet.... Ill make one for you... ')
+        config_file = open('config.txt', "w+")
+        print('Ok its made but its empty because the file got deleted or was never there.\n')
+        do_next()
+
+
+def resume():
+    global dict_of_info
+    if not bool(dict_of_info):
+        print('Nothing to Resume')
+        time.sleep(2)
+        main()
+
+    else:
+        what_do_want_to_do()
+
+
+def main():
+    os.system("clear")
+    print('Main Menu'.center(50, '_'), '\n')
+    print('1.[A]dd Some Text to Repeat')
+    print('2.[V]iew or Use Saved')
+    print('3.[R]esume')
+    print('4.[Q]uit')
+    main_input = input('Input : ')
+    if main_input in ('1', 'a'):
+        add_text()
+    elif main_input in ('2', 'v'):
+        view_saved()
+    elif main_input in ('3', 'r'):
+        resume()
+    elif main_input in ('4', 'q'):
+        quit()
+    else:
+        main()
+
+main()
--- README.md
+++ README.md
@@ -1,29 +1,174 @@
-## PASTE_O_ MATIC
+## PASTE_O_ MATIC v2
 
-Simple Python3 script for inputting text which you wish to repeat over and over.  
+Simple Python3 "3.6" script for inputting text which you wish to repeat over and over.  
 Various forms of output options.  
 New line or Same line.  
-Your text is output to a file if you wish to save it.  
-Your text can also be typed for you using PyAutoGUI for "realistic human" input.   
-You can change the amount of time until spamming will begin so that you have time to move 
-to whatever text field you need and you can also change the interval at which each character is typed.  
-This can be useful for adjusting for spamming filters in various chat applications.  
+Copy the text to your clipboard.  
+Save to a file with a custom name.  
+Save it for later.  
+Printing it in the terminal.  
+The ability to spam it.  
 
-## Dependencies  
+
+### Dependencies  
 
 This is a simple script so there's no point in me making a install for it however the dependencies for the script are certainly worth installing.  
 
-latest versions  
+Latest Versions  
 
 clipboard  
 pyautgui  
 
+#Main Menu
 
-## Warning!!! and Disclaimer!!!
-Because of the sheer laziness of this script, the text that is generated is both stored on the disk and in memory.
-If the text you generate takes more memory or disk space then you have... You'er probably going to have a bad time. 
-There is no multi-threading so this is likely to hammer one core. 
-The text file is generated before reading it into memory. 
-What's more, this is very inefficient code and is going to be accessing the file for every paste. 
-So as a Disclaimer I take no responsibility any damage this script may cause and I suggest you read it over first.
-With all that being said if you're just trying to spam your Bae with 1000 kisses you're probably good to go.
+First you are greeted with this menu.  
+
+    ____________________Main Menu_____________________ 
+    1.[A]dd Some Text to Repeat
+    2.[V]iew or Use Saved
+    3.[R]esume
+    4.[Q]uit
+    Input : 
+
+In this menu you can choose whether you want to...
+
+####Add some text to be repeated.  
+This will then ask you for some text input and then ask you how many times he would like it to be repeated and then it will ask you whether you want the text to be repeated on the same line or the next line.  It will then send you to the  output options menu aka  (What do you want to do next menu) NOTE: Your text must include any spaces you want and cannot contain any new lines. You can experiment but I'm thinking ASCII art is not going to be supported unfortunately. You may also encounter problems with unicode.
+
+####View and or  use saved text  
+This will take you to the view saved menu in which you can view text along with it's parameters that you have saved in the config file. This will give you the option to return to the main menu or choose a number from the list which he would like to load. Each saved item will show you the full python dictionary for the text and parameters that you have created.
+
+####Resume your session  
+If there is a session to resume meaning there is a text and parameters loaded in memory this section will dump you straight into the output menu so you can choose what to do with it.
+
+####Quit - ends the script
+
+
+
+##Output Menu ( "What do you want to do next" menu)
+
+     ______________What Do You Want to Do______________ 
+
+    OK I'll Repeat that 1 Times 
+
+    How to Output it
+    1. [C]lipboard
+    2. [F]ile.txt
+    3. [S]ave For Later
+    4. [J]ust SPAM IT!
+    5. [P]rint Here...
+    6. [M]ain Menu 
+    Your Output Choice : 
+
+In this menu you will be given output options for your text.
+
+####Clipboard  
+This will simply copy the repeated texts to your clipboard so that you can paste it in wherever you would like. It will then give you the option to quit or go to the main menu at which point you could choose to use resume to Output the text again in some manner as well as save it.
+
+####File.txt   
+This will allow you to Output the repeated text to a custom named text file and give you the ability to go back to the main menu or quit.
+
+####Save for later  
+Using safe for later you will be able to save the text and it's parameters for repeating to the config file.
+
+####Just Spam it  
+Just Spam it offers you a variety of options for spamming the text. **Please see that section of the documentation**.
+
+####Print Here  
+Print here does as the name would suggest. Printing the text in the terminal or command line.
+
+####Main Menu - Sends you back to the main menu
+
+##Spamming  
+As the name suggests spamming gives you the ability to spam text in a variety of ways. Please see examples.
+
+Examples  
+Spamming with _NEXT LINE_  on "Your Text"
+
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    
+Spamming with _SAME LINE_ on "Your Text " <-- Note the space
+
+    Your Text Your Text Your Text Your Text Your Text Your Text 
+
+You will be asked to input some parameters for text spamming. These parameters include:
+
+**Interval** - Of time between each character being typed.  
+
+**Delay** -  Which is useful for scenarios that involve anti-spam measures such as restricting the time in between messages.  
+
+**Number** - Of seconds until start -  To give yourself time to move to the correct window and text field.  
+
+And then you will be presented with two options.
+
+###Repeated  
+Will be your text repeated the number of times you have specified with a Enter / Send  after each.  See examples.  
+
+Examples  
+Spamming with _NEXT LINE_  on "Your Text"
+
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    #Enter key pressed here
+"#Enter key pressed here" line is not printed.
+
+Spamming with _SAME LINE_ on "Your Text " <-- Note the space
+
+    Your Text Your Text Your Text Your Text Your Text Your Text 
+    #Enter key pressed here
+"#Enter key pressed here" line is not printed.
+
+###Spam  
+Will take the entirety of your repeated text at the number that you have specified  then press Enter / Send and will repeat this action a number that you will specify. See examples.  
+
+ Examples  
+ Spamming with _NEXT LINE_  on "Your Text" spammed "3" times
+ 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    #Enter key pressed here
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    #Enter key pressed here
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    Your Text 
+    #Enter key pressed here
+"#Enter key pressed here" line is not printed.  
+
+Spamming with _SAME LINE_ on "Your Text " <-- Note the space. spammed "3" times
+
+    Your Text Your Text Your Text Your Text Your Text Your Text 
+    #Enter key pressed here
+    Your Text Your Text Your Text Your Text Your Text Your Text 
+    #Enter key pressed here
+    Your Text Your Text Your Text Your Text Your Text Your Text 
+    #Enter key pressed here
+"#Enter key pressed here" line is not printed.  
+
+You will then be offered the ability to begin the spam by pressing enter. And depending on your choices you will also at the end of the spam be given the ability to spam again quit or go back to the Main Menu.
+
+NOTE: Depending on the text field Behavior May differ. Using the "new line" option in your configuration May cause certain text fields to send that message. However the Enter / Send  is sending a key press "enter" so depending on the text field you will have to experiment with your options.  This is why I have tried to make this flexible.
+
+##!!!Warning & Disclaimer!!!
+There's no built-in limiter so you could quite easily create a set of parameters that would cause the memory to fill up as well as potentially disk space. There is no multi-threading so this is going to hammer One Core for the entire job. so this is use at your own risk. I take no responsibility for misuse of this script and I do not condone any illegal use of the script. I highly suggest you read the script before using it to familiarize yourself with what it's doing.
